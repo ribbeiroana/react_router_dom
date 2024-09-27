d4e73598c6a1abd1784d7afce0035bb4bff8728e
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-09-27 11:30:01 -0300

    arquivos

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+import './style.css';
+
+export const Home = () => {
+  return (
+  <div>
+    <h1>Olá</h1>
+  </div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/index.css'
+import { Home } from './components/Home'
+import { About } from './components/About'
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Menu } from './components/Menu'
+import { Posts } from './components/Posts'
+import { Post } from './components/Post'
+import { Redirect } from './components/Redirect'
+import { NotFound } from './components/NotFound'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Menu/>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path="/posts" element={<Posts />}>
+      <Route path=":id" element={<Post />} />
+      </Route>
+      <Route path="/posts" element={<Posts />} />
+      <Route path='/redirect' element={<Redirect />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
